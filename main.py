924be0f144693bace172f67ef62e5ad518e30e72
ThomasEsayas <tomesayas6@gmail.com>
2023-11-17 12:26:14 -0600

    fixed calc_f to multiple masses

--- main.py
+++ main.py
@@ -12,48 +12,43 @@ dt = 0.001
 G = 6.67430 * 10 ** -11
 
 # initial conditions
-m = [5.9 * 10 ** 24, 7.34 * 10 ** 22]
-init_a = [Vector(0, 0), Vector(0, 0)]
-init_v = [[Vector(0, 0)], [Vector(0, 0)]]
-init_x = [Vector(0, 0), Vector(384000000, 0)]
+m = [5.9 * 10 ** 24, 7.34 * 10 ** 22, 10]
+init_a = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
+init_v = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
+init_x = [Vector(0, 0), Vector(10, 0), Vector(20, 0)]
+
 x = init_x
+v = init_v
+a = init_a
 
 # creates pre-subtracted radii vectors
 # list comprehension of radii to a body
 
-radii = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
-     [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if x[i] != x[j]] for i in range(len(x))]]
+calc_r = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
+     [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if i != j] for i in range(len(x))]]
 
-r = radii("penis")
-
-print(f"radii: {radii(1)}")
+r = calc_r('')
 
 # list comprehension of forces of each body
-forces = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
+calc_f = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
 
+f = calc_f('')
 
-f = forces("penis")
-
-print(f"forces: {f}")
+# Kinematics Section
+# Acceletation
 
 # list comprehension of accelerations
-accs = lambda _: [[(f[i][j] / m[i]) for j in range(len(f[i]))] for i in range(len(f))]
+calc_a = lambda _: [[(f[i][j] / m[i]) for j in range(len(f[i]))] for i in range(len(f))]
 
-a_prev = init_a
-a = accs("penis")
-
-
-print(f"accelerations: {a}")
+a_prev = a
+a = calc_a('')
 
 # list comprehension of velocities
-print(a_prev)
-print(a)
-vels = init_v
-vels = lambda _: [[(0.5*(a_prev[i][j]+a[i][j]))*dt for j in range(len(f[i]))] for i in range(len(f))]
+calc_v = lambda _: [[(0.5*(a_prev[i][j]+a[i][j]))*dt for j in range(len(f[i]))] for i in range(len(f))]
 
-v = vels("penis")
-
-print(f"velocities: {v}")
+v_prev = v
+pprint(calc_v(''))
+v = calc_v("penis")
 
 end_time = time.time()
 execution_time = end_time - start_time
