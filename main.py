08b28660de32e55e76a61f974e6971c30a3bb643
Spython20 <71393868+Spython20@users.noreply.github.com>
2023-11-17 13:34:39 -0600

    created so i can edit thomas' changes

--- main.py
+++ main.py
@@ -8,52 +8,60 @@ start_time = time.time()
 t = 0
 i = 0
 t_end = 100
-dt = 1
+dt = 0.001
 G = 6.67430 * 10 ** -11
 
 # initial conditions
-m = [5.9 * 10 ** 24, 7.34 * 10 ** 22]
-init_a = [[Vector(0, 0)], [Vector(0, 0)]]
-init_v = [[Vector(0, 0)], [Vector(0, 0)]]
-init_x = [Vector(0, 0), Vector(384000000,0)]
+m = [5.9 * 10 ** 24, 7.34 * 10 ** 22, 10]
+init_a = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
+init_v = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
+init_x = [Vector(0, 0), Vector(384 * 10 ** 6, 0), Vector(20, 0)]
+
 x = init_x
+v = init_v
+a = init_a
 
 # creates pre-subtracted radii vectors
 # list comprehension of radii to a body
 
-radii = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
-     [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if x[i] != x[j]] for i in range(len(x))]]
+calc_r = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
+     [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if i != j] for i in range(len(x))]]
 
-r = radii("penis")
-
-print(f"radii: {radii(1)}")
+r = calc_r('')
 
 # list comprehension of forces of each body
-forces = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
+calc_f = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
 
+f = calc_f('')
 
-f = forces("penis")
-
-print(f"forces: {f}")
-
+# Kinematics Section
+# Acceletation
+print(f'sum: {sum([Vector(0,0),Vector(0,0)])}')
 # list comprehension of accelerations
-accs = lambda _: [[(f[i][j] / m[i]) for j in range(len(f[i]))] for i in range(len(f))]
+calc_a = lambda _: [sum([(f[i][j] / m[i]) for j in range(len(f[i]))]) for i in range(len(f))]
 
-a_prev = init_a
-a = accs("penis")
-
-
-print(f"accelerations: {a}")
+a_prev = a
+a = calc_a('')
 
 # list comprehension of velocities
-print(a_prev)
-print(a)
-vels = init_v
-vels = lambda _: [[(0.5*(a_prev[i][j]+a[i][j]))*dt for j in range(len(f[i]))] for i in range(len(f))]
+calc_v = lambda _: 1 / 2 * np.array([(np.array(a_prev[i])+np.array(a[i])) * dt for i in range(len(f))])
 
-v = vels("penis")
+v_prev = v
+v += calc_v('')
+# list comprehension of positions
+calc_x = lambda _: np.array([np.array(v_prev[i]) * dt + 1 / 2 * np.array(a_prev[i]) * dt ** 2 for i in range(len(f))])
 
-print(f"velocities: {v}")
+x_prev = x
+x += calc_x('')
+
+pprint(f'r: {r}')
+pprint(f'f: {f}')
+pprint(f'a_prev: {a_prev}')
+pprint(f'a: {a}')
+pprint(f'v_prev: {v_prev}')
+pprint(f'v: {v}')
+pprint(f'x_prev: {x_prev}')
+pprint(f'x: {x}')
 
 end_time = time.time()
 execution_time = end_time - start_time
