62658e5c7466539e5c5e6f48a554af0e719cc94b
Spython20 <71393868+Spython20@users.noreply.github.com>
2023-11-18 01:41:02 -0600

    began implementing pygame

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from vector import Vector
+from planet import Planet
 import numpy as np
 from pprint import pprint
 import time
+import pygame
 
 start_time = time.time()
 
 t = 0
 i = 0
 t_end = 100
-dt = 0.001
+dt = 0.01
 G = 6.67430 * 10 ** -11
 
 # initial conditions
@@ -16,94 +18,96 @@ m = [5.9 * 10 ** 24, 7.34 * 10 ** 22, 10]
 init_a = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
 init_v = [Vector(0, 0), Vector(0, 0), Vector(0, 0)]
 init_x = [Vector(0, 0), Vector(384 * 10 ** 6, 0), Vector(20, 0)]
+r_body = [8, 8, 8]
+
+data = []
 
 x = init_x
 v = init_v
 a = init_a
 
-# creates pre-subtracted radii vectors
-# list comprehension of radii to a body
+for t in np.arange(0, t_end, dt):
+    # creates pre-subtracted radii vectors
+    # list comprehension of radii to a body
 
-calc_r = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
-     [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if i != j] for i in range(len(x))]]
+    calc_r = lambda _: [{k: v for sub_dict in sub_array for k, v in sub_dict.items()} for sub_array in
+        [[{f"{i}{j}": (x[j] - x[i])} for j in range(len(x)) if i != j] for i in range(len(x))]]
 
-r = calc_r('')
+    r = calc_r('')
 
-# list comprehension of forces of each body
-calc_f = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
+    # list comprehension of forces of each body
+    calc_f = lambda _: [[G * m[i] * sum([m[j] * r[i][f'{i}{j}'] / r[i][f"{i}{j}"].length() ** 3]) for j in range(len(x)) if i != j] for i in range(len(x))]
 
-f = calc_f('')
+    f = calc_f('')
 
-# Kinematics Section
-# Acceletation
-print(f'sum: {sum([Vector(0,0),Vector(0,0)])}')
-# list comprehension of accelerations
-calc_a = lambda _: [sum([(f[i][j] / m[i]) for j in range(len(f[i]))]) for i in range(len(f))]
+    # Kinematics Section
+    # Acceletation
+    # list comprehension of accelerations
+    calc_a = lambda _: [sum([(f[i][j] / m[i]) for j in range(len(f[i]))]) for i in range(len(f))]
 
-a_prev = a
-a = calc_a('')
+    a_prev = a
+    a = calc_a('')
 
-# list comprehension of velocities
-calc_v = lambda _: 1 / 2 * np.array([(np.array(a_prev[i])+np.array(a[i])) * dt for i in range(len(f))])
+    # list comprehension of velocities
+    calc_v = lambda _: (1 / 2) * np.array([(np.array(a_prev[i])+np.array(a[i])) * dt for i in range(len(f))])
 
-v_prev = v
-v += calc_v('')
-# list comprehension of positions
-calc_x = lambda _: np.array([np.array(v_prev[i]) * dt + 1 / 2 * np.array(a_prev[i]) * dt ** 2 for i in range(len(f))])
+    v_prev = v
+    v += calc_v('')
+    # list comprehension of positions
+    calc_x = lambda _: np.array([np.array(v_prev[i]) * dt + 1 / 2 * np.array(a_prev[i]) * dt ** 2 for i in range(len(f))])
 
-x_prev = x
-x += calc_x('')
+    x_prev = x
+    x += calc_x('')
+    data.append( {
+        't': t,
+        'x': x,
+        'v': v,
+        'a': a,
+        'f': f,
+        'r': r
+    })
 
-pprint(f'r: {r}')
-pprint(f'f: {f}')
-pprint(f'a_prev: {a_prev}')
-pprint(f'a: {a}')
-pprint(f'v_prev: {v_prev}')
-pprint(f'v: {v}')
-pprint(f'x_prev: {x_prev}')
-pprint(f'x: {x}')
-
+pprint(data[0]['x'][0])
 end_time = time.time()
 execution_time = end_time - start_time
 
 print("Execution time:", execution_time)
 
-'''
-# THIS WORKS BUT I WANT SINGLE LINE LAMBDAS ONLY :))))))
-def compute_accelerations(f):
-    for i in range(len(f)):
-        for j in range(len(f[i])):
-            f[i][j] = (f[i][j] / m[i]) + init_a[i]
-
-    return f
-
-print(compute_accelerations(f))
-'''
-
-
-def update_positions(r, v, a, dt, i):
-    pass
-
-
-def update_velocities(v, a, dt, i):
-    pass
-
-
-def print_final_positions():
-    pass
-
-
 """
-while t < t_end:
-    for body in a:
-        a[body] = compute_accelerations(r, i)
-    for body in r:
-        r[body] = update_positions(r, v, a, dt, i)
-    for body in a:
-        a[body] = compute_accelerations(r, i)
-    for body in v:
-        v[body] = update_velocities(v, a, dt, i)
-    t = t + dt
-    # (print diagnostics here?)
-print_final_positions()
+THIS IS WHERE THE ANIMATION BEGINS
 """
+
+screen = pygame.display.set_mode((960, 720))
+planets = []
+
+for i in range(len(m)):
+    planets.append(Planet(init_x[i], r_body[i]))
+
+
+print(planets)
+pygame.init()
+
+app_running = True
+
+def draw():
+    screen.fill((50, 50, 50))
+
+#def update():
+
+
+while app_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            app_running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                app_running = False
+
+
+    draw()
+
+    #update()
+
+    pygame.display.flip()
+
+pygame.quit()
